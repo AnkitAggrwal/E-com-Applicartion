10297f560db0c1feb33bd8aa280277e66414c8df
Ankit Aggarwal <akkuaggarwal22@gmail.com>
2026-05-10 21:59:50 +0530

    Implement user management with User, UserController, and UserService classes

--- Ecom-application/src/main/java/com/app/ecom/user/User.java
+++ Ecom-application/src/main/java/com/app/ecom/user/User.java
@@ -1,12 +1,29 @@
 package com.app.ecom.user;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 @Entity(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
+
     private String firstName;
+
     private String lastName;
+
+    public User() {
+    }
+
+    public User(Long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
